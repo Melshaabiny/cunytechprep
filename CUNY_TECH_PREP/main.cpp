790f2dc0480fded148a69a197d16e7d1f06a8696
MElshaabiny <moustafa.elshaabiny@gmail.com>
2015-09-22 14:05:20 -0400

    Initial Commit

---
+++ CUNY_TECH_PREP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CUNY_TECH_PREP
+//
+//  Created by Moustafa ElShaabiny on 9/22/15.
+//  Copyright (c) 2015 Moustafa ElShaabiny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
